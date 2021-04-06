e28e38620271fbe4bb4e5bbecc4060c8d15d87eb
CUPZYY <richbanana1234@gmail.com>
2021-04-06 16:55:11 +0200

    added non python version

---
+++ non python version/gui.py
@@ -0,0 +1,30 @@
+from tkinter import *
+from scipy.sparse.csgraph import _validation
+
+# from tkinter import ttk
+
+class guiClass():
+
+    def gui(self):
+        root = Tk()
+        # def command():
+        #    root.destroy()
+        #    exit()
+
+        root.geometry("270x75")
+        root.resizable(False, False)
+        root.title("EMU Login")
+        root.iconbitmap("toaster.ico")
+        root['background'] = '#F0F0F0'
+        heading = Label(text="Communicating with Mega. Please wait...", font=("Segoe", 10), fg="black", height="2")
+        heading.place(relx=0.5, rely=0.4, anchor=CENTER)
+        # SOON:
+        # register = ttk.Button(root, text="Cancel", command=command)
+        # register.pack()
+
+        root.mainloop()
+
+        def terminate():
+            root.destroy()
+
+
